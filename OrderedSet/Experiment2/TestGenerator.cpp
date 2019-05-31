ddc73c661383e49c80e4cf5fb06da898cad716ed
BohdanPastuschak <bohdanpastuschak@gmail.com>
2019-05-31 23:27:52 +0300

    Create TestGenerator.cpp

---
+++ OrderedSet/Experiment2/TestGenerator.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	freopen("In.txt", "w", stdout);//write data to file for testing
+	
+	int n = 10000000;
+	printf("%d\n", n);
+	int size = 0;//total size of set now
+	int select = 0;
+	int insert = 0;
+	int rank = 0;
+	unordered_set<int> used;//for already used numbers
+	for (int i = 0; i < n; i++)
+	{
+		int type = rng() % 3;
+		int k;
+		if (type == 0 || size == 0)
+		{
+			type = 0;
+			k = rng() % INT_MAX;
+			while(used.count(k) > 0)
+				k = rng() % INT_MAX;
+			
+			used.insert(k);	
+			size++;
+			insert++;
+		}
+		else
+		{
+			if (type == 1)
+			{
+				select++;
+				k = rng() % size;
+			}
+			else
+			{
+				rank++;
+				k = rng() % INT_MAX;
+			}
+		}	
+		
+		printf("%d %d\n", type, k);
+	}	
+	
+	cerr << insert << ' ' << select << ' ' << rank << endl;
+	return 0;
+}
