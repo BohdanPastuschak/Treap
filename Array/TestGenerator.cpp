8750fc2aff34ff37bd5bdb1199818a5703d2fccd
BohdanPastuschak <bohdanpastuschak@gmail.com>
2019-06-10 19:04:16 +0300

    Update TestGenerator.cpp

--- Array/TestGenerator.cpp
+++ Array/TestGenerator.cpp
@@ -3,36 +3,40 @@ using namespace std;
 
 int main()
 {
-	int n, m;
-	n = 1000000;
-	m = 1000000;
-	cout << n << ' ' << m << endl;
-	for (int i = 0; i < n; i++)
-		cout << rand() % 10000 << ' ';
-	cout << endl;
-	
-	while(m--)
+	int t = 10;
+	printf("%d\n", t);
+	while(t--)
 	{
-		int type = rand() % 7;
-		type++;
-		cout << type << " ";
-		int l = rand() % n;
-		int r = rand() % n;
-		if (l > r)
-			swap(l, r);
-			
-		if (type == 1 || type == 4 || type == 6 || type == 7)
-			cout << l << ' ' << r << endl;
-		else
+		int n, m;
+		n = 1000000;
+		m = 1000000;
+		printf("%d %d\n", n, m);
+		for (int i = 0; i < n; i++)
+			printf("%d ", rand() % 10000);
+		printf("\n");
+		
+		while(m--)
 		{
-			if (type == 2 || type == 5)
-				cout << l << ' ' << r << ' ' << (rand() % 10000) << endl;
+			int type = rand() % 7;
+			type++;
+			printf("%d ", type);
+			int l = rand() % n;
+			int r = rand() % n;
+			if (l > r)
+				swap(l, r);
+				
+			if (type == 1 || type == 4 || type == 6 || type == 7)
+				printf("%d %d\n", l, r);
 			else
-				cout << l << ' ' << r << ' ' << (rand() % (n - (r - l))) << endl;
-		}		
+			{
+				if (type == 2 || type == 5)
+					printf("%d %d %d\n", l, r, rand() % 10000);
+				else
+					printf("%d %d %d\n", l, r, rand() % (n - r + l));
+			}		
+		}
 	}
 	
-	//cerr << "Time elapsed: " << clock() / (double)CLOCKS_PER_SEC << endl;
+	cerr << "Time elapsed: " << clock() / (double)CLOCKS_PER_SEC << endl;
 	return 0;
 }
-
