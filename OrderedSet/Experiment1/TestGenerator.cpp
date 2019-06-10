c5b92d180857a22d721943da3336cd37fcec5527
BohdanPastuschak <bohdanpastuschak@gmail.com>
2019-06-10 21:10:21 +0300

    Update TestGenerator.cpp

--- OrderedSet/Experiment1/TestGenerator.cpp
+++ OrderedSet/Experiment1/TestGenerator.cpp
@@ -4,38 +4,65 @@ using namespace std;
 int main()
 {
 	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-	freopen("In.txt", "w", stdout);//write data to file for testing
 	
-	int n = 10000000;
-	printf("%d\n", n);
-	int size = 0;//total size of set now
-	int select = 0;
-	int insert = 0;
-	unordered_set<int> used;//for already used numbers
-	for (int i = 0; i < n; i++)
+	int tests = 10;
+	printf("%d\n", tests);
+	while(tests--)
 	{
-		int type = rng() & 1;
-		int k;
-		if (type == 0 || size == 0)
-		{
-			type = 0;
-			k = rng() % INT_MAX;
-			while(used.count(k) > 0)
-				k = rng() % INT_MAX;
-			
-			used.insert(k);	
-			size++;
-			insert++;
-		}
-		else
-		{
-			select++;
-			k = rng() % size;
-		}	
-		
-		printf("%d %d\n", type, k);
-	}	
+		int n = 3000000;
+		printf("%d\n", n);
+		int size = 0;//total size of set now
 	
-	cerr << insert << ' ' << select << endl;
+		set<int> used;//for already used numbers
+		for (int i = 0; i < n; i++)
+		{
+			int type = rng() % 100;
+			int k;
+			
+			int percentInsert = 50;
+			int percentErase = 0;
+			int percentSelect = 50;
+			
+			if (type < percentInsert || size == 0)
+			{
+				type = 0;
+				k = rng() % INT_MAX;
+				while(used.count(k) > 0)
+					k = rng() % INT_MAX;
+				
+				used.insert(k);	
+				size++;
+			}
+			else
+			{
+				type -= percentInsert;
+				if (type < percentErase)
+				{
+					type = 3;
+					k = rng() % INT_MAX;
+					k = *used.lower_bound(k);
+					used.erase(k);
+					size--;
+				}
+				else
+				{
+					type -= percentErase;
+					if (type < percentSelect)
+					{
+						type = 1;
+						k = rng() % size;
+					}
+					else
+					{
+						type = 2;
+						k = rng() % INT_MAX;
+					}
+				}
+			}	
+			
+			printf("%d %d\n", type, k);
+		}	
+	}
+	
 	return 0;
 }
