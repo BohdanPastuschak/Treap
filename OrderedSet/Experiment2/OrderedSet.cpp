924d5b2ea16b7188e9825f6f9a0ea187765e8bed
BohdanPastuschak <bohdanpastuschak@gmail.com>
2019-06-10 21:50:40 +0300

    Update OrderedSet.cpp

--- OrderedSet/Experiment2/OrderedSet.cpp
+++ OrderedSet/Experiment2/OrderedSet.cpp
@@ -14,40 +14,52 @@ ordered_set;
 
 int main()
 {	
-	ordered_set s;//making cartesian tree
-	int n;//number of operations
-	scanf("%d", &n);
-	long long result = 0;//in order to not output many values, 
-						 //let's calcutlate only some hash function 
-						 //to see whether our results in both programs are equal
-	
-	for (int i = 0; i < n; i++)
+	int tests;
+	scanf("%d", &tests);
+	for(int test = 0; test < tests; ++test)
 	{
-		int type;//type of operation: 0 for insert, 1 for select
-		scanf("%d", &type);
-		if (type == 0)
-		{
-			int k;//value to insert
-			scanf("%d", &k);
-			s.insert(k);//inserting new value
-		}		
+		ordered_set s;
+		int n;//number of operations
+		scanf("%d", &n);
 		
-		if (type == 1)
+		for (int i = 0; i < n; i++)
 		{
-			int k;//value to select
-			scanf("%d", &k);
-			result += *s.find_by_order(k);//selecting k
-		}
-		
-		if (type == 2)
-		{
-			int k;//value to rank
-			scanf("%d", &k);
-			result += s.order_of_key(k);//rank of k
-		}
-	}	
+			int type;//type of operation: 0 for insert, 1 for select, 2 for rank, 3 for erase
+			scanf("%d", &type);
+			if (type == 0)
+			{
+				int k;//value to insert
+				scanf("%d", &k);
+				s.insert(k);//inserting new value
+			}		
+			
+			if (type == 1)
+			{
+				int k;//value to select
+				scanf("%d", &k);
+				printf("%d\n", *s.find_by_order(k));//selecting k
+			}
+			
+			if (type == 2)
+			{
+				int k;//value to rank
+				scanf("%d", &k);
+				printf("%d\n", (int)s.order_of_key(k));//rank of k
+			}
+			
+			if (type == 3)
+			{
+				int k;//value to erase
+				scanf("%d", &k);
+				s.erase(k);//erasing value
+			}	
+		}	
+	}
 	
-	printf("%lld\n", result);
-	cerr << "Time elapsed: " << clock() / (double)CLOCKS_PER_SEC << endl;//to measure total time
+	double time = clock() / (double) CLOCKS_PER_SEC;
+	double average = time / tests;
+	
+	cerr << "Time elapsed: " << time << endl;
+	cerr << "Average time: " << average << endl;
 	return 0;
 }
