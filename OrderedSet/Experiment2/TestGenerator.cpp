aa2dc06266261675120d296ff64d27a3b8012d2a
BohdanPastuschak <bohdanpastuschak@gmail.com>
2019-06-10 21:50:51 +0300

    Update TestGenerator.cpp

--- OrderedSet/Experiment2/TestGenerator.cpp
+++ OrderedSet/Experiment2/TestGenerator.cpp
@@ -4,47 +4,65 @@ using namespace std;
 int main()
 {
 	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-	freopen("In.txt", "w", stdout);//write data to file for testing
 	
-	int n = 10000000;
-	printf("%d\n", n);
-	int size = 0;//total size of set now
-	int select = 0;
-	int insert = 0;
-	int rank = 0;
-	unordered_set<int> used;//for already used numbers
-	for (int i = 0; i < n; i++)
+	int tests = 10;
+	printf("%d\n", tests);
+	while(tests--)
 	{
-		int type = rng() % 3;
-		int k;
-		if (type == 0 || size == 0)
+		int n = 10000000;
+		printf("%d\n", n);
+		int size = 0;//total size of set now
+	
+		set<int> used;//for already used numbers
+		for (int i = 0; i < n; i++)
 		{
-			type = 0;
-			k = rng() % INT_MAX;
-			while(used.count(k) > 0)
-				k = rng() % INT_MAX;
+			int type = rng() % 100;
+			int k;
 			
-			used.insert(k);	
-			size++;
-			insert++;
-		}
-		else
-		{
-			if (type == 1)
+			int percentInsert = 34;
+			int percentErase = 0;
+			int percentSelect = 33;
+			
+			if (type < percentInsert || size == 0)
 			{
-				select++;
-				k = rng() % size;
+				type = 0;
+				k = rng() % INT_MAX;
+				while(used.count(k) > 0)
+					k = rng() % INT_MAX;
+				
+				used.insert(k);	
+				size++;
 			}
 			else
 			{
-				rank++;
-				k = rng() % INT_MAX;
-			}
+				type -= percentInsert;
+				if (type < percentErase)
+				{
+					type = 3;
+					k = rng() % INT_MAX;
+					k = *used.lower_bound(k);
+					used.erase(k);
+					size--;
+				}
+				else
+				{
+					type -= percentErase;
+					if (type < percentSelect)
+					{
+						type = 1;
+						k = rng() % size;
+					}
+					else
+					{
+						type = 2;
+						k = rng() % INT_MAX;
+					}
+				}
+			}	
+			
+			printf("%d %d\n", type, k);
 		}	
-		
-		printf("%d %d\n", type, k);
-	}	
+	}
 	
-	cerr << insert << ' ' << select << ' ' << rank << endl;
 	return 0;
 }
